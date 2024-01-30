2241732988c566a0192c5e6f68e7d1f785468af9
fishyW <winstonwijaya88@gmail.com>
2024-01-31 00:25:50 +0700

    test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::de::IntoDeserializer;
+use tauri::{http::{header::{CONTENT_LENGTH, CONTENT_TYPE}, Request, Response, ResponseBuilder}, AppHandle, Wry};
+use url::Url;
+use std::{fs::{self, File}, io::Read};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello {}!", name)
@@ -10,6 +15,22 @@ fn greet(name: &str) -> String {
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
+    .register_uri_scheme_protocol("test",  |_app, req| {
+
+    // decode the URL
+      let buf = fs::read(req.uri()
+        .strip_prefix("test:/")
+        .unwrap())
+        .unwrap();      
+
+        println!("{}", String::from_utf8(buf.clone()).unwrap());
+
+      tauri::http::ResponseBuilder::new()
+        .header("Access-Control-Allow-Origin", "*")
+        .mimetype("text/javascript")
+        .status(200)
+        .body(buf)
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,47 @@
-<script>
-    import Greet from "$lib/Greet.svelte";
+<script lang="ts">
+	import { open } from '@tauri-apps/api/dialog';
+	import { convertFileSrc } from '@tauri-apps/api/tauri';
+	import { onMount } from 'svelte';
+	import { fetch } from '@tauri-apps/api/http';
+	// $: {
+	//     (async () => {
+	//         if (js == null) {return;}
+	//         let module = await import(/* @vite-ignore*/ js);
+	//         console.log("s");
+	//         module.test();
+	//     })();
+	// }
+
+	let file = '';
+
+  const textUrl = "/home/winsst/Downloads/empty.txt";
+  let text = "";
+	const ok = async () => {
+		const res = '/home/winsst/Downloads/test.js';
+		file = convertFileSrc(res, "test");
+    try {
+      let data = await fetch("https://test.localhost");
+    //   console.log(data);
+
+    } catch (e) {
+      console.error(e);
+    }
+    // await fetch(convertFileSrc(res, "javascript"));
+	};
+
+  onMount(async () => {
+	const path = "/home/winsst/Downloads/test.js"
+	const mod = await import(`test:/${path}`);
+
+  })
 </script>
 
-<h1>Welcome to SvelteKit!</h1>
-<Greet />
+
+<h1>Welcome to SvelteKt!</h1>
+
+<p>Hello</p>
+<p>Test</p>
+
+<embed src={text} />
+
+<button on:click={ok}>Open</button>
