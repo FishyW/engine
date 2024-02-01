236c44d6971337f67cfe8b1416eca4cae6fb4a03
fishyW <winstonwijaya88@gmail.com>
2024-02-02 00:29:13 +0700

    completed the init modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,85 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { exportWasm } from "$lib/utils";
-	
+	import InitModal from "$lib/components/InitModal.svelte";
+	// import type * as path from '@tauri-apps/api/path';
+	import { convertFileSrc } from '@tauri-apps/api/tauri';
 
-	export let data;
+	import {type Wasm} from "$lib/path";
+	import { modifyGlobalFetch } from '$lib/utils';
+
+	const PROTOCOL = "fetch";
+
+	// project/engine/wasm
+	const wasmPathSuffix = ["engine", "wasm"];
+	const wasmJSName = "project.js";
+
+	// variables
 	let display = "";
+	let projectPath = "";
+	let showInitModal = true;
 
-	$: wasm = data.wasm!;
-	
+	// dynamic modules
+	let tauriPath: typeof import("@tauri-apps/api/path");
+	let wasm: Wasm;
 
-	function addWorld(str: string) {
-		return str + " world!";
+	// tauriPath module, needs to be dynamically imported
+	// see https://github.com/tauri-apps/tauri/discussions/5271
+	onMount(async () => {
+		tauriPath = await import("@tauri-apps/api/path");
+	});
+
+	// promise wrapper -> makes a promise returns the tuple [data, error]
+	// this prevents an annoying try catch block
+	async function pwrap<T>(promise: Promise<T>): Promise<[T|null, null|string|Error]> {
+		try {
+			return [await promise, null];
+		} catch (e) {
+			return [null, e as string | Error];
+		}
 	}
 
-	onMount(() => {
-		// exports addWorld() to wasm, putting it under the "hello" namespace
-		exportWasm({addWorld}, "hello");
+	async function initializeWasm(wasmPath: string) {
 
+		// converts path to url
+		const file = convertFileSrc(await tauriPath.join(wasmPath, wasmJSName), PROTOCOL);
+	
+		// dynamic import the file
+		wasm = await import(/* @vite-ignore */ file);
+	
+		// modifies the fetch function to work with wasm
+		modifyGlobalFetch(wasmPath, PROTOCOL);
+
+		// call init(), wasm.default() is init()
+		await wasm.default();		
+	}
+
+	async function onSelectionEnd(event: CustomEvent<any>) {
+		const projectPath: string = event.detail;
+
+		const wasmPath = await tauriPath.join(projectPath, ...wasmPathSuffix);
+		const [_, err] = await pwrap(initializeWasm(wasmPath));
+
+		if (err != null) {
+			alert("Invalid project folder!");
+			console.error(err);
+			return;
+		}
+
+		showInitModal = false;
 		display = wasm.init_script();
-		document.body.style.display = "revert";
-	})
+	}
+
 
 </script>
 
+{#if showInitModal}
+<InitModal on:selection_end={onSelectionEnd} />
+{/if}
 
-<h1>Welcome to SvelteKt!</h1>
+{#if display}
+<h1>Hello World in Action!</h1>
 <p>{display}</p>
-
+{/if}
 
 <svelte:window on:keypress={e => {
 
@@ -37,6 +90,17 @@
 
 <style>
 	:global(body) {
-		display: none;
+		margin: 0;
+		height: 100vh;
+		width: 100vw;
+		box-sizing: border-box;
 	}
+
+	h1, p {
+		margin: 10px;
+	}
+
+
+
+
 </style>
